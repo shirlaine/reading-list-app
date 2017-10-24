897076191c2ef197f7e1b3fd11c5d50fd309c38a
shirlaine <shirlaine.phang@gmail.com>
2017-10-24 22:19:17 +0800

    Add devise and edit controller to use current_user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024032919) do
+ActiveRecord::Schema.define(version: 20171024135657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20171024032919) do
     t.string "title"
     t.string "author"
     t.boolean "read", default: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
