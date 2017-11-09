9222b94c339da6032adf9fe929a3ea809afa86a8
shirlaine <shirlaine.phang@gmail.com>
2017-11-09 20:57:54 +0800

    Add migration file, images to books, add images of books to index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024135657) do
+ActiveRecord::Schema.define(version: 20171109123452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20171024135657) do
     t.string "author"
     t.boolean "read", default: false
     t.integer "user_id"
+    t.string "images"
   end
 
   create_table "users", force: :cascade do |t|
